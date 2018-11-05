e547469db85320beb0f9cd8fff897fcc3ee2a0ea
changhee oh <och8808@gmail.com>
2018-11-05 05:40:58 +0000

    app.py 설정

---
+++ app.py
@@ -0,0 +1,20 @@
+# /app.py
+from flask import Flask
+# flask ORM
+from flask_sqlalchemy import SQLAlchemy
+# 마이그레이션 관리
+from flask_migrate import Migrate
+
+# 모델 설정파일 import
+from models import *
+
+app = Flask(__name__)
+# DB 설정과 관련된 코드
+# 'postgresql:///<pqsl에서 만든 DATABASE이름>'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///myboard'
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "hello world"
